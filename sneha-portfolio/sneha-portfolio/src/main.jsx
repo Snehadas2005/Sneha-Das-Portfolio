12ed03cb6e51c047ac2bcf5a96143929ae84e0f5
Sneha Das <154408198+Snehadas2005@users.noreply.github.com>
2025-09-15 15:56:16 +0530

    Intial commit

--- sneha-portfolio/sneha-portfolio/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/routes";
-import "./styles/index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
-);
